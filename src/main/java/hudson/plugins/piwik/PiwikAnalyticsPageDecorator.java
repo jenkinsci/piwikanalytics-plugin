24ade045ce9815f010c318c2912e7ed357cb9a04
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2013-03-17 19:39:13 +0100

    use PathHelper + refactor

--- src/main/java/hudson/plugins/piwik/PiwikAnalyticsPageDecorator.java
+++ src/main/java/hudson/plugins/piwik/PiwikAnalyticsPageDecorator.java
@@ -4,77 +4,86 @@ import hudson.Extension;
 import hudson.Util;
 import hudson.model.PageDecorator;
 import net.sf.json.JSONObject;
-
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
 @Extension
 public class PiwikAnalyticsPageDecorator extends PageDecorator {
 
-	private String siteId;
-	private String piwikServer;
-	private String piwikPath;
-	private String additionnalDownloadExtensions;
+    public static final String PLUGIN_DISPLAY_NAME = "Piwik Analytics";
+    private String siteId;
+    private String piwikServer;
+    private String piwikPath;
+    private String additionnalDownloadExtensions;
+    private boolean useAsync = false;
 
-	public PiwikAnalyticsPageDecorator() {
-		super(PiwikAnalyticsPageDecorator.class);
-		load();
-	}
+    public PiwikAnalyticsPageDecorator() {
+        super();
+        load();
+    }
 
-	@DataBoundConstructor
-	public PiwikAnalyticsPageDecorator(String _siteId, String _piwikServer,
-			String _piwikPath, String _additionnalDEx) {
-		this();
-		this.siteId = _siteId;
-		this.piwikServer = _piwikServer;
-		this.piwikPath = _piwikPath;
-		this.additionnalDownloadExtensions = _additionnalDEx;
-	}
+    @DataBoundConstructor
+    public PiwikAnalyticsPageDecorator(String siteId, String piwikServer,
+                                       String piwikPath, String additionnalDEx, boolean useAsync) {
+        this();
+        setSiteId(siteId);
+        setPiwikServer(piwikServer);
+        setPiwikPath(piwikPath);
+        setAdditionnalDownloadExtensions(additionnalDEx);
+        setUseAsync(useAsync);
+    }
 
-	@Override
-	public String getDisplayName() {
-		return "Piwik Analytics";
-	}
+    @Override
+    public String getDisplayName() {
+        return PLUGIN_DISPLAY_NAME;
+    }
 
-	@Override
-	public boolean configure(StaplerRequest req, JSONObject json)
-			throws FormException {
-		req.bindJSON(this, json);
-		save();
-		return true;
-	}
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject json)
+            throws FormException {
+        req.bindJSON(this, json);
+        save();
+        return true;
+    }
 
-	public String getSiteId() {
-		return Util.fixEmpty(siteId);
-	}
+    public String getSiteId() {
+        return siteId;
+    }
 
-	public void setSiteId(String siteId) {
-		this.siteId = siteId;
-	}
+    public void setSiteId(String siteId) {
+        this.siteId = Util.fixEmpty(siteId);
+    }
 
-	public String getPiwikServer() {
-		return Util.fixEmpty(piwikServer);
-	}
+    public String getPiwikServer() {
+        return piwikServer;
+    }
 
-	public void setPiwikServer(String piwikServer) {
-		this.piwikServer = piwikServer;
-	}
+    public void setPiwikServer(String piwikServer) {
+        this.piwikServer = PathHelper.fixPath(piwikServer);
+    }
 
-	public String getPiwikPath() {
-		return Util.fixEmpty(piwikPath);
-	}
+    public String getPiwikPath() {
+        return piwikPath;
+    }
 
-	public void setPiwikPath(String piwikPath) {
-		this.piwikPath = piwikPath;
-	}
+    public void setPiwikPath(String piwikPath) {
+        this.piwikPath = PathHelper.fixPath(piwikPath);
+    }
 
-	public String getAdditionnalDownloadExtensions() {
-		return additionnalDownloadExtensions;
-	}
+    public String getAdditionnalDownloadExtensions() {
+        return additionnalDownloadExtensions;
+    }
 
-	public void setAdditionnalDownloadExtensions(
-			String additionnalDownloadExtensions) {
-		this.additionnalDownloadExtensions = additionnalDownloadExtensions;
-	}
+    public void setAdditionnalDownloadExtensions(
+            String additionnalDownloadExtensions) {
+        this.additionnalDownloadExtensions = additionnalDownloadExtensions;
+    }
 
+    public boolean isUseAsync() {
+        return useAsync;
+    }
+
+    public void setUseAsync(boolean useAsync) {
+        this.useAsync = useAsync;
+    }
 }
